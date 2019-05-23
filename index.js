b2eef90641684424a21dcb92eaae76a6601b944c
Austin Jess <ajess33@gmail.com>
2019-05-23 16:26:50 -0500

    async functionality implemented

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// const fileReader = require('./lib/reader.js');
+const fileReader = require('./lib/reader.js');
 const asyncFileReader = require('./lib/reader-async.js');
+const promiseFileReader = require('./lib/reader-promises.js');
+const editFile = require('./lib/edit-file.js');
 
 // Obtain and assert input
 const files = process.argv.slice(2);
@@ -8,11 +10,21 @@ if (!(files instanceof Array && files.length)) {
   throw new Error('Invalid Args');
 }
 
-// fileReader(files, (err, data) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log('From Callback:', data);
-// });
+fileReader(files, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log('From Callback:', data);
+});
 
 asyncFileReader(files);
+
+promiseFileReader(files);
+const fileToEdit = process.argv.slice(2).toString();
+
+editFile(fileToEdit, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log('After Data - ', data);
+});
